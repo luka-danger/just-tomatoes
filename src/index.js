77dc18f1fd103220ea5ee81e5d9567539f80c608
luka-danger <lukathevegan@gmail.com>
2024-09-21 10:32:27 -0600

    Push initial commits

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+
+function tomatoCount() {
+    for (let i = 1; i < 10; i++) {
+        if (i == 1) {
+            console.log(`${i} tomato.`)
+        }
+        else {
+            console.log(`${i} tomatoes.`)
+        }    
+    }
+}
+
+
